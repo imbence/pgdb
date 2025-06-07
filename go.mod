2436243bd2d3d6a42b46b81beb66beee3663c783
Bence <snbence@gmail.com>
2025-06-07 09:32:28 +0200

    code

--- go.mod
+++ go.mod
@@ -1,3 +1,30 @@
-module pg_db
+module github.com/imbence/pg_db
 
-go 1.24
+go 1.24.1
+
+require (
+	github.com/jackc/pgx/v5 v5.7.5
+	github.com/uptrace/bun v1.2.11
+	github.com/uptrace/bun/dialect/pgdialect v1.2.11
+	github.com/uptrace/bun/driver/pgdriver v1.2.11
+	github.com/uptrace/bun/extra/bundebug v1.2.11
+)
+
+require (
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/puzpuzpuz/xsync/v3 v3.5.1 // indirect
+	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	go.opentelemetry.io/otel v1.34.0 // indirect
+	go.opentelemetry.io/otel/trace v1.34.0 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+	mellium.im/sasl v0.3.2 // indirect
+)
--- pg_db.go
+++ pg_db.go
@@ -1,1 +1,76 @@
-package main
+package pg_db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	_ "github.com/jackc/pgx/v5"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
+	"github.com/uptrace/bun/extra/bundebug"
+	"strings"
+)
+
+var (
+	DB  *bun.DB
+	Ctx context.Context
+)
+
+func ConnectToDb(dbUrl string) error {
+	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dbUrl)))
+	DB = bun.NewDB(sqldb, pgdialect.New())
+	if err := DB.Ping(); err != nil {
+		return err
+	}
+	DB.AddQueryHook(bundebug.NewQueryHook(bundebug.WithVerbose(true)))
+	Ctx = context.Background()
+	return nil
+}
+
+func ToDb[T any](data []T, table string, schema string) (int64, error) {
+	modelTableExpr := fmt.Sprintf("%s.%s", schema, table)
+
+	onConflictString, primaryKeyString, err := getTableColumns(table, schema)
+	if err != nil {
+		return 0, err
+	}
+	sqlRes, err := DB.NewInsert().
+		ModelTableExpr(modelTableExpr).
+		Model(&data).
+		On(fmt.Sprintf("CONFLICT (%s) DO UPDATE", primaryKeyString)).
+		Set(onConflictString).
+		Exec(Ctx)
+	if err != nil {
+		return 0, err
+	}
+	return sqlRes.RowsAffected()
+}
+
+func getTableColumns(table, schema string) (string, string, error) {
+	var columns []string
+	var primaryKey []string
+
+	if err := DB.NewSelect().
+		ColumnExpr("column_name").
+		TableExpr("information_schema.columns").
+		Where("table_name = ? and table_schema = ?", table, schema).
+		Scan(Ctx, &columns); err != nil {
+		return "", "", err
+	}
+
+	if err := DB.NewSelect().
+		ColumnExpr("column_name").
+		TableExpr("information_schema.key_column_usage").
+		Where("table_name = ? and table_schema = ?", table, schema).
+		Scan(Ctx, &primaryKey); err != nil {
+		return "", "", err
+	}
+
+	excludedColumns := make([]string, 0, len(columns))
+	for _, col := range columns {
+		excludedColumns = append(excludedColumns, fmt.Sprintf("%s = excluded.%s", col, col))
+	}
+
+	return strings.Join(excludedColumns, ", "), strings.Join(primaryKey, ", "), nil
+}
